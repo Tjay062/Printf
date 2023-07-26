65d0de01bd1226fa6a36756804ae854e27ae557c
Tommytinny <tommyteenboi@gmail.com>
2023-07-26 01:24:33 +0100

    update

--- utils.c
+++ utils.c
@@ -12,6 +12,7 @@ int is_printable(char c)
 		return (1);
 	return (0);
 }
+
 /**
  * append_hexa_code - Append ascci in hexadecimal code to buffer
  * @buffer: Array of chars.
@@ -22,13 +23,12 @@ int is_printable(char c)
 int append_hexa_code(char ascii_code, char buffer[], int i)
 {
 	char map_to[] = "0123456789ABCDEF";
-
+	/* The hexa format code is always 2 digits long */
 	if (ascii_code < 0)
-	{
 		ascii_code *= -1;
-	}
-	buffer[i++] = 'x';
+
 	buffer[i++] = '\\';
+	buffer[i++] = 'x';
 
 	buffer[i++] = map_to[ascii_code / 16];
 	buffer[i] = map_to[ascii_code % 16];
@@ -46,14 +46,15 @@ int is_digit(char c)
 {
 	if (c >= '0' && c <= '9')
 		return (1);
+
 	return (0);
 }
 
 /**
  * convert_size_number - Casts a number to the specified size
  * @num: Number to be casted.
- *
  * @size: Number indicating the type to be casted.
+ *
  * Return: Casted value of num
  */
 long int convert_size_number(long int num, int size)
@@ -82,4 +83,3 @@ long int convert_size_unsgnd(unsigned long int num, int size)
 
 	return ((unsigned int)num);
 }
-
